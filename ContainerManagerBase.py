6ca26eb0034b9624a6e21e5327db1d824d70e3d7
Clemens Elflein <clemens1@familie-elflein.de>
2024-05-30 19:27:07 +0200

    Container logs working

---
+++ ContainerManagerBase.py
@@ -0,0 +1,90 @@
+import logging
+import json
+import podman.domain.containers
+from podman import PodmanClient
+
+
+class ContainerManagerBase:
+    """
+    Base class for managing containers using Podman.
+    This class connects to Podman, looks for a container with name `managed_container_name`
+    and gets a handle to it, if exists.
+
+    Args:
+        podman_client (PodmanClient): The Podman client object.
+        managed_container_name (str): The name of the container being managed.
+        logger (logging.Logger): The logger object for logging messages.
+
+    Attributes:
+        logger (logging.Logger): The logger object for logging messages.
+        _podman_client (PodmanClient): The Podman client object.
+        managed_container_name (str): The name of the container being managed.
+        _container_handle (podman.domain.containers.Container | None): The handle to the managed container, if it exists.
+
+    """
+    def __init__(self, podman_client: PodmanClient, managed_container_name: str, logger: logging.Logger):
+        self.logger = logger
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self._podman_client = podman_client
+        self.managed_container_name = managed_container_name
+        self._container_handle: podman.domain.containers.Container | None = None
+        # Check, if a container already exists, if so, fetch the handle.
+        for container in self._podman_client.containers.list(all=True, filters={"name": self.managed_container_name}):
+            self._container_handle = container
+            self.logger.info(f"Found container: {container.name}")
+            break
+
+    def pull_image(self, repository, tag) -> str | None:
+        """
+        :param repository: The name of the repository from which to pull the image.
+        :param tag: The tag of the image to pull.
+        :return: The image ID of the pulled image, or None if the image failed to be pulled.
+
+        This method is used to pull an image from a docker repository.
+
+        Example usage:
+        ```
+        image_id = pull_image("myrepository", "latest")
+        if image_id:
+            print("Image pulled successfully.")
+        else:
+            print("Failed to pull the image.")
+        ```
+        """
+        self.logger.info(f"Pulling Image {repository}:{tag}")
+        image_id = None
+        for line in self._podman_client.images.pull(repository, tag, stream=True):
+            # Stream is JSON dicts, decode
+            d = json.loads(line)
+            if "stream" in d:
+                self.logger.debug(d["stream"])
+            elif "images" in d:
+                image_id = d["id"]
+        self.logger.info(f"Image {repository}:{tag} pulled successfully.")
+        return image_id
+
+    def wait(self, conditions: [str]):
+        """
+        :param conditions: A list of conditions that the method waits for.
+        :return: None
+        """
+        if self._container_handle is None:
+            return
+        while True:
+            self._container_handle.wait(condition=conditions, interval="1000ms")
+            self._container_handle.reload()
+            if self._container_handle.status in conditions:
+                break
+            self.logger.info(
+                f"Container status is {self._container_handle.status}, expected: {conditions} waiting some more."
+            )
+
+    def stream_logs(self):
+        """
+        Stream Docker container logs.
+
+        :return: Docker container logs stream or None, if the container is not active.
+        """
+        if self._container_handle is None:
+            return None
+        return self._container_handle.logs(stderr=True, stdout=True, stream=True, follow=True)
--- main.py
+++ main.py
@@ -1,270 +1,133 @@
+import asyncio
 import logging
 import sys
-from typing import List, Dict
+import threading
+import time
+from contextlib import asynccontextmanager
 
-import podman.domain.containers
-from fastapi import FastAPI
-import json
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 from podman import PodmanClient
-from pydantic import BaseModel
+from ContainerManager import ContainerManager
+from MetaContainerManager import MetaContainerManager
 
-app = FastAPI()
+logging.basicConfig(level=logging.INFO, format='%(asctime)s -%(levelname)s -on line: %(lineno)d -%(message)s')
 
 pc = PodmanClient(base_url="unix:///run/user/1000/podman/podman.sock")
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
 handler.setStream(sys.stdout)
 logger.addHandler(handler)
 
-print("yoooooooooooo")
+logger.setLevel(logging.DEBUG)
 
+meta_manager = MetaContainerManager(pc, "meta", logger)
+mower_container_manager = ContainerManager(
+    pc, "open-mower", "ghcr.io/clemenselflein/open_mower_ros", "latest", [],
+    {
+        "mounts": [{
+            "type": "bind",
+            "source": "/home/clemens/mower_config.sh",
+            "target": "/config/mower_config.sh"
+        }]
+    }, logger
+)
 
-class ContainerManagerBase:
-    def __init__(self, podman_client: PodmanClient, managed_container_name: str):
-        self._podman_client = podman_client
-        self.managed_container_name = managed_container_name
-        self._container_handle: podman.domain.containers.Container | None = None
-        # Check, if a container already exists, if so, fetch the handle.
-        for container in self._podman_client.containers.list(all=True, filters={"name": self.managed_container_name}):
-            self._container_handle = container
-            logger.info(f"Found container: {container.name}")
-            break
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    def pull_image(self, repository, tag) -> str | None:
-        logger.info(f"Pulling Image {repository}:{tag}")
-        image_id = None
-        for line in self._podman_client.images.pull(repository, tag, stream=True):
-            # Stream is JSON dicts, decode
-            d = json.loads(line)
-            if "stream" in d:
-                logger.debug(d["stream"])
-            elif "images" in d:
-                image_id = d["id"]
-        logger.info(f"Image {repository}:{tag} pulled successfully.")
-        return image_id
-
-    def wait(self, conditions: [str]):
-        while True:
-            self._container_handle.wait(condition=conditions, interval="1000ms")
-            self._container_handle.reload()
-            if self._container_handle.status in conditions:
-                break
-            logger.info(
-                f"Container status is {self._container_handle.status}, expected: {conditions} waiting some more.")
-
-
-class MetaContainerManager(ContainerManagerBase):
-    def __init__(self, podman_client: PodmanClient, managed_container_name: str):
-        super().__init__(podman_client, managed_container_name)
-
-    def update(self) -> bool:
-        # Update the meta container, if we don't have a handle, we cannot update
-        if self._container_handle is None:
-            logger.warning("No container handle, cannot update meta container image.")
-            return False
-        # Fetch image repo and tag from the container handle and pull
-        image_repo, tag = self._container_handle.attrs["Image"].split(":")
-        self.pull_image(image_repo, tag)
-        self._container_handle.stop()
-        return True
-
-
-class ContainerManager(ContainerManagerBase):
-
-    def __init__(self, podman_client: PodmanClient, managed_container_name: str, repository: str, tag: str,
-                 command: List[str], props: Dict):
-        super().__init__(podman_client, managed_container_name)
-        self.repository = repository
-        self.tag = tag
-        self.command = command
-        self.props = props
-
-    def stop_container(self):
-        if self.__container_handle is None:
-            return
-        logger.info(f"Stopping and removing container.")
-        self.__container_handle.stop(ignore=True)
-        self.wait(["stopped", "exited"])
-
-        logger.info(f"Container {self.managed_container_name} stopped.")
-        self.__container_handle.remove()
-        logger.info(f"Container {self.managed_container_name} removed.")
-        self.__container_handle = None
-        return
-
-    def get_image_id(self, force_pull: bool = False) -> str | None:
-        image_id = None
-        if not self._podman_client.images.exists(f"{self.repository}:{self.tag}") or force_pull:
-            logger.info(f"Image {self.repository}:{self.tag} not found, pulling.")
-
-        else:
-            logger.debug(f"Image {self.repository}:{self.tag} found locally, skipping pull.")
-            image_id = self._podman_client.images.get(f"{self.repository}:{self.tag}").id
-        return image_id
-
-    def get_logs(self):
-        if self.__container_handle is None:
-            return [""]
-        return [line.strip() for line in self.__container_handle.logs(stderr=True, stdout=True)]
-
-    def update(self) -> bool:
-        # If we don't have a handle, we cannot update.
-        if self.__container_handle is None:
-            return False
-
-        # Pull the image
-        self.get_image_id(True)
-        self.__container_handle.restart()
-
-    def create(self) -> bool:
-        self.stop_container()
-        image_id = self.get_image_id()
-        if image_id is None:
-            logger.error("Error getting image.")
-            return False
-        self.__container_handle = self._podman_client.containers.create(image=image_id, command=self.command,
-                                                                        name=self.managed_container_name, **self.props)
-        logger.info("Starting Container.")
-        self.__container_handle.start()
-        self.wait(["running", "exited"])
-        if self.__container_handle.status == "running":
-            logger.info("Container Started.")
-            return True
-        else:
-            logger.error("Error starting container.")
-            for line in self.get_logs():
-                logger.error(line)
-            self.stop_container()
-            return False
-
-meta_manager = MetaContainerManager(pc, "meta")
-
-# if __name__ == '__main__':
-
-    # mower_container = ContainerManager(pc, "open-mower", "ghcr.io/clemenselflein/open_mower_ros", "latest", [], {
-    #     "mounts": [{
-    #         "type": "bind",
-    #         "source": "/home/clemens/mower_config.sh",
-    #         "target": "/config/mower_config.sh"
-    #     }]
-    # })
-    # mower_container.update()
-    # mower_container.create()
-
-    # for c in pc.containers.list(all=True):
-    #     logger.info(f"removing and stopping {c.name}")
-    #     c.stop(ignore=True)
-    #     c.wait(condition=["stopped", "exited"])
-    #     c.remove()
-
-    # version = podman_client.version()
-    # print("Release: ", version["Version"])
-    # print("Compatible API: ", version["ApiVersion"])
-    # print("Podman API: ", version["Components"][0]["Details"]["APIVersion"], "\n")
-
-    # get all images
-    # print("images:")
-    # for image in podman_client.images.list():
-    #     print(image, image.id, "\n")
-    #     repo, id = image.attrs.get("RepoTags")[0].split(":")
-    #     podman_client.images.pull(repo, id)
-
-    # images = podman_client.images.list()
-    # print("pulling image")
-    # image_id = ""
-    # for line in podman_client.images.pull("ghcr.io/clemenselflein/open_mower_ros", "latest", stream=True):
-    #     d = json.loads(line)
-    #     if "stream" in d:
-    #         print(d["stream"])
-    #     elif "images" in d:
-    #         image_id = d["id"]
-    # print("image pulled")
-
-    # repository = "ghcr.io/clemenselflein/open_mower_ros"
-    # tag = "releases-edge"
-    #
-    # image_id = None
-    # if not podman_client.images.exists(f"{repository}:{tag}"):
-    #     print(f"image {repository}:{tag} not found locally, pulling.")
-    #     for line in podman_client.images.pull("ghcr.io/clemenselflein/open_mower_ros", "latest", stream=True):
-    #         d = json.loads(line)
-    #         if "stream" in d:
-    #             print(d["stream"])
-    #         elif "images" in d:
-    #             image_id = d["id"]
-    #     print("image pulled")
-    # else:
-    #     print(f"image {repository}:{tag} found locally.")
-    #     image_id = podman_client.images.get(f"{repository}:{tag}").id
-    #
-    # raise Exception(f"ID: {image_id}")
-    # container = podman_client.containers.create(image=image_id, command=["asdf"], restart_policy={"Name": "always"},
-    #                                             ports={"8080": "8080"})
-    # print("container created")
-    # container.start()
-    # container.wait(condition="running")
-    # print("container running")
-    #
-    # print("containers:")
-    # # find all containers
-    # for container in podman_client.containers.list():
-    #     # container.restart()
-    #     logs = container.logs()
-    #     for line in container.logs():
-    #         print(line)
-    #
-    # print(json.dumps(podman_client.df(), indent=4))
-#
 @app.post("/update-self")
 async def update_self():
     meta_manager.update()
-    return {"message": "Hello World"}
+    return "OK"
 
-@app.get("/version")
-async def get_version():
-    return "v6"
 
-#
-#
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     return {"message": f"Hello {name}"}
-#
-#
-# @app.get("/images")
-# async def get_images():
-#     version = podman_client.version()
-#     print("Release: ", version["Version"])
-#     print("Compatible API: ", version["ApiVersion"])
-#     print("Podman API: ", version["Components"][0]["Details"]["APIVersion"], "\n")
-#
-#     # get all images
-#     # print("images:")
-#     # for image in podman_client.images.list():
-#     #     print(image, image.id, "\n")
-#     #     repo, id = image.attrs.get("RepoTags")[0].split(":")
-#     #     podman_client.images.pull(repo, id)
-#
-#     images = podman_client.images.list()
-#     print("pulling image")
-#     image = podman_client.images.pull("ghcr.io/clemenselflein", "open_mower_ros:latest")
-#     print("image pulled")
-#     container = podman_client.containers.create(image=image.id, command=["asdf"], restart_policy={"Name": "always"}, ports={"8080":"8080"})
-#     print("container created")
-#     container.start()
-#     container.wait(condition="running")
-#     print("container running")
-#
-#     print("containers:")
-#     # find all containers
-#     for container in podman_client.containers.list():
-#         # container.restart()
-#         logs = container.logs()
-#         for line in container.logs():
-#             print(line)
-#
-#     print(json.dumps(podman_client.df(), indent=4))
-#     return "OK"
+@app.get("/container/status")
+async def get_container_status():
+    return mower_container_manager.status()
+
+
+@app.post("/container/action")
+def post_container_action(action: str):
+    print(action)
+    if action == "start":
+        mower_container_manager.create()
+    elif action == "stop":
+        mower_container_manager.stop_container()
+    elif action == "pull":
+        mower_container_manager.update()
+    return mower_container_manager.status()
+
+
+@app.websocket("/ws/container/logs")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+
+    await websocket.send_text("Log Websocket Connected.")
+
+    # Keep a buffer of lines before sending out in order to not spam the websocket
+    lines: [str] = []
+
+    # Podman steam is not asyncio, so we need to run in separate executor
+    def stream_data(cancel_event: threading.Event):
+        while not cancel_event.is_set():
+            gen = mower_container_manager.stream_logs()
+            if gen is not None:
+                # Stream the lines returned from the generator, cancel on event
+                for line in gen:
+                    lines.append(line.decode("utf-8"))
+                    if cancel_event.is_set():
+                        break
+                logger.debug("closing generator")
+                gen.close()
+            if not cancel_event.is_set():
+                # Generator stopped, but event is not set - retry after some time
+                logger.debug("stream ended, but not stopped; retrying")
+                time.sleep(1)
+
+    # A flag to stop the podman stream
+    stream_cancel_event = threading.Event()
+    loop = asyncio.get_event_loop().run_in_executor(None, stream_data, stream_cancel_event)
+
+    # Also stream the manager's logs, so that we can see image pulls etc
+    class Handler(logging.Handler):
+        def __init__(self):
+            logging.Handler.__init__(self)
+
+        def emit(self, record):
+            lines.append(f"[GUI] {self.format(record)}")
+
+    stream_handler = Handler()
+    stream_handler.setLevel(logging.INFO)
+    mower_container_manager.logger.addHandler(stream_handler)
+    try:
+        while True:
+            # Limit update rate to the websocket
+            await asyncio.sleep(0.1)
+            # need to send something in order to check if the websocket is still alive
+            # see: https://github.com/tiangolo/fastapi/discussions/9031
+            try:
+                await asyncio.wait_for(
+                    websocket.receive(), 0.1
+                )
+            except asyncio.TimeoutError:
+                pass
+            if len(lines) > 0:
+                await websocket.send_text(str.join("\r\n", lines))
+                lines.clear()
+    except Exception as e:
+        logger.debug(f"Websocket exception: {e}")
+        logger.removeHandler(stream_handler)
+        stream_cancel_event.set()
+        # Wait for the podman stream to finish
+        await loop
+        logger.debug("WS stream done")
+
